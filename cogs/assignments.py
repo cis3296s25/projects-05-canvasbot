94349d346631e6e385be75e5422e6ee9a8d08f0d
Jaryn Hernandez <jarynh14@gmail.com>
2025-04-15 11:59:57 -0400

    AutoAssignments feature: Users will not be sent a dm of overdue assignmnets of the past 5 days. Both DMS will now ONLY DISPLAY UNSUBMITTED assignments.

--- cogs/assignments.py
+++ cogs/assignments.py
@@ -78,9 +78,12 @@ class autoAssignmentNotify(commands.Cog):
                             due = due.replace(tzinfo=pytz.utc)
                             now = dt.now(pytz.utc) 
 
-                            if 0 <= (due - now).days <= 5:
-                                assignment.course_name = courses[course.id].name
-                                assignments.append(assignment)
+                            if 0 <= (due - now).days <= 7:
+                                submission = assignment.get_submission('self')
+                                if not submission or (submission.workflow_state == "unsubmitted" and not getattr(submission, "excused", False)):
+                                    assignment.course_id = course.id
+                                    assignment.course_name = courses[course.id].name
+                                    assignments.append(assignment)
                 except Exception as e:
                     print(f"Error fetching assignments for {course.name}: {e}")
         except Exception as e:
@@ -89,20 +92,56 @@ class autoAssignmentNotify(commands.Cog):
         return assignments
 
 
-    '''
-    Check for assignments every x seconds (for development) 
-    check for assignments every 24 hours (for production)
+    def overdue_assignments(self, api_key):
+        '''
+        check for assignments that are overdue from the last 5 days 
+        '''
+        canvas = canvasapi.Canvas(API_URL, api_key)
+        overdueAssignments = []
 
-    This function will retrieve the users credentials from users.json
-    and then check for assignments for each user. If there are any assignments
-    due in the next x days, the user will be messaged with the assignments.
-    '''
+        try:
+            courses = {course.id: course for course in canvas.get_courses(enrollment_state='active')}
+            for course in courses.values():
+                try:
+                    for assignment in course.get_assignments():
+                        if assignment.due_at:
+                            due = dt.strptime(assignment.due_at, '%Y-%m-%dT%H:%M:%SZ')
+                            due = due.replace(tzinfo=pytz.utc)
+                            now = dt.now(pytz.utc) 
+
+                            if 0 <= (now - due).days <= 5:
+                                submission = assignment.get_submission('self')
+                                if not submission or (submission.workflow_state == "unsubmitted" and not getattr(submission, "excused", False)):
+                                    assignment.course_name = courses[course.id].name
+                                    overdueAssignments.append(assignment)
+
+                except Exception as e:
+                    print(f"Error fetching assignments for {course.name}: {e}")
+        except Exception as e:
+            print(f"Error fetching courses: {e}")
+
+        return overdueAssignments
+
     @tasks.loop(hours=24)
-    async def check_assignments(self):
+    async def check_assignments(self):    
+        '''
+        Check for assignments every x seconds (for development) 
+        check for assignments every 24 hours (for production)
+
+        This function will retrieve the users credentials from users.json
+        and then check for assignments for each user. If there are any assignments
+        due in the next x days, the user will be messaged with the assignments.
+        '''
         users = await self.get_users()  
 
         for snowflake, api_key in users.items(): 
             assignments = self.get_assignments(api_key)
+            overdue_assignments = self.overdue_assignments(api_key)
+
+            if overdue_assignments:
+                user = await self.client.fetch_user(snowflake)
+                if user:
+                    await self.overdue_message(user, overdue_assignments, api_key)
 
             if assignments:
                 user = await self.client.fetch_user(snowflake)
@@ -112,7 +151,7 @@ class autoAssignmentNotify(commands.Cog):
 
     '''
     This function will contruct the message (pre-formatted) to be sent to the user
-    with the assignments that are due in the next 5 days.
+    with the assignments that are due in the next 7 days.
     
     Canvas API does not proivde the course name for each assignment, so we have to
     manually add it to the assignment with course id.
@@ -131,20 +170,43 @@ class autoAssignmentNotify(commands.Cog):
                 courseAssignments[course_name] = []
 
             courseAssignments[course_name].append(assignment)
-
-        message = self.format_message(courseAssignments)
         
+        message = self.format_message(courseAssignments)
+
         try:
-            await user.send(message)
+            for chunk in self.split_message(message):
+                await user.send(chunk)
         except Exception as e:
             print(f"Failed to send DM: {e}")
 
+    async def overdue_message(self, user, overdueAssignments, api_key):
+        courseAssignments = {}
+
+        canvas = canvasapi.Canvas(API_URL, api_key)
+        users_courses = {course.id: course for course in canvas.get_courses(enrollment_state='active')}
+
+        for assignment in overdueAssignments:
+            course_id = assignment.course_id
+            course_name = users_courses.get(course_id, {}).name if course_id in users_courses else "Unknown Course"
+
+            if course_name not in courseAssignments:
+                courseAssignments[course_name] = []
+
+            courseAssignments[course_name].append(assignment)
+        
+        overdueMessage = self.format_message_overdue(courseAssignments)
+        
+        try:
+            for chunk in self.split_message(overdueMessage):
+                await user.send(chunk)
+        except Exception as e:
+            print(f"Failed to send DM: {e}")
 
     '''
-    Format the message to be sent to the user with the assignments that are due in the next 5 days.
+    Format the messages to be sent to the user with the assignments that are due in the next 7 days.
     '''
     def format_message(self, courseAssignments):
-        message = "**Upcoming Assignments for the Next 5 Days!**\n\n"
+        message = "**Upcoming Unsubmitted Assignments for the Next 7 Days!**\n\n"
         for course, assignments in courseAssignments.items():
             message += f"**{course}**\n"
             for assignment in assignments:
@@ -156,6 +218,35 @@ class autoAssignmentNotify(commands.Cog):
 
 
         return message
+    
+    
+    '''
+    Format the messages to be sent to the user with the assignments that are past due in the last 5 days.
+    '''
+    def format_message_overdue(self, courseAssignments):
+        message = "**Overdue Assignments from the past 5 Days!**\n\n"
+        for course, assignments in courseAssignments.items():
+            message += f"**{course}**\n"
+            for assignment in assignments:
+                due = dt.strptime(assignment.due_at, '%Y-%m-%dT%H:%M:%SZ').replace(tzinfo=pytz.utc)
+                due_local = due.astimezone(pytz.timezone('US/Eastern'))
+                due_str = due_local.strftime('%m/%d/%Y at %I:%M %p') 
+                message += f"[{assignment.name}]({assignment.html_url}) - **Due:** {due_str}\n"
+            message += "\n"
+
+        return message
+    
+
+    def split_message(self, message, limit=2000):
+        chunks = []
+        while len(message) > limit:
+            split_index = message.rfind('\n', 0, limit)
+            if split_index == -1:
+                split_index = limit
+            chunks.append(message[:split_index])
+            message = message[split_index:].lstrip()
+        chunks.append(message)
+        return chunks 
 
     @check_assignments.before_loop
     async def before_check_assignments(self):
